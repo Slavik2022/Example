7be3459cf0835c989e2f6eb0e5e0d2f6df3a3269
Slavik <soldatslavik@gmail.com>
2022-10-14 18:47:50 +0300

    Example005_Ifelse

---
+++ Example005_Ifelse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "слава")
+{
+    Console.WriteLine("Ура, это же СЛАВА");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
