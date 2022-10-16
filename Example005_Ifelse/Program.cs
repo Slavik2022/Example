fbce2d18ebe1439c2a85e8b449edaac7b2cef672
Slavik <soldatslavik@gmail.com>
2022-10-16 21:32:57 +0300

    Add Task 008-011

--- Example005_Ifelse/Program.cs
+++ Example005_Ifelse/Program.cs
@@ -1,10 +1,11 @@
 ﻿Console.Write("Введите имя пользователя");
 string username = Console.ReadLine();
-if(username.ToLower() == "слава")
+if (username.ToLower() == "слава")
 {
     Console.WriteLine("Ура, это же СЛАВА");
 }
-else{
+else
+{
     Console.Write("Привет, ");
     Console.WriteLine(username);
 }
---
+++ example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 76, 89, 75, 83, 5, 65, 82, 23, 5 };
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index +1;
+    index++;
+};
